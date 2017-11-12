1e71a533aea371327e318120a9c92a8c797904f8
ShivamKumarJha <jha.shivam3@gmail.com>
2017-11-12 07:51:13 +0530

    tomato: Set BOARD_KERNEL_IMAGE_NAME

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,4 +1,5 @@
 # Kernel
+BOARD_KERNEL_IMAGE_NAME := zImage
 LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_CONFIG := lineageos_tomato_defconfig
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
